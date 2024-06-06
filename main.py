b69f3298c78e3876c095350aef58d72e2b0f7640
JohnPaulPrabhu <jpprabhu33@gmail.com>
2024-06-06 09:22:28 +0530

    Added the check to track vehicle and detect license plate

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from ultralytics import YOLO
 import cv2
+import numpy as np
+from sort.sort import *
+
 
 def main():
+    motion_tracker = Sort()
+    
     # load the number plate detector model
-    model = YOLO("number-plate-detector.pt")
+    license_plate_detector = YOLO("number-plate-detector.pt")
     coco_model = YOLO("yolov8n.pt")
     
     # load the video
@@ -16,51 +21,38 @@ def main():
         total+=1
         ret, frame = cap.read()
         # detect vehivlcle
-        if total > 10:
+        if total >= 1:
             break
         if ret:
             detections = coco_model(frame)[0]
+            detections_ = []
             for detection in detections.boxes.data.tolist():
-                print(detection)
-            # print(detections)
+                x1, y1, x2, y2, confidence_score, class_id = detection
+                if confidence_score in detect:
+                    detections.append([x1, y1, x2, y2, confidence_score])
+    
+            # track vehicle
+            motion_tracker.update(np.asarray(detections_))
+    
+            # detect license plate
+            license_plates = license_plate_detector(frame)[0]
+            for license_plate in license_plates.boxes.data.tolist():
+                x1, y1, x2, y2, confidence_score, class_id = license_plate
+    
+    
+            # assign license plate to car
+    
+   
+            # crop license plate
+
+    
+            # process license plate
             
-    
-    
-    
-    
-    
-    # track vehicle
-    
-    
-    
-    
-    # detect license plate
-    
-    
-    
-    
-    
-    # assign license plate to car
-    
-    
-    
-    
-    
-    # crop license plate
-    
-    
-    
-    
-    
-    # process license plate
-    
 
-
-    # read license plate number
-
-
-
-    # write results 
+            # read license plate number
+        
+        
+            # write results 
     
     
     
